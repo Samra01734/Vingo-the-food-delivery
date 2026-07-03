a8a8861c53ba11d7b90463365884b716ed771d1d
Samra <samramuqadasvu1@gmail.com>
2026-07-03 06:31:28 +0500

    day3

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,28 @@
 
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 const signUp =async (req ,res )=>{
     try {
-        
+        const {fullName,email ,password,mobile,role}=req.body
+        const user=await User.findOne({emaial})
+        if(user){
+            return res.status(400).json({message:"User Already exist.."})
+        }
+        if (password.length <6){
+        return res.status(400).json({message:"Password must be atleast 6 characters .."})
+ 
+        }
+        if (mobile.length<10){
+          return res.status(400).json({message:"Mobile number must be at least 10 digits.."})
+        }
+        const hashPassword=await bcrypt.hash(password,10)
+        user=await User.create({
+            fullName,
+            email,
+            role,
+            moboile,
+            password:hashPassword
+        })
     } catch (error) {
         
     }
